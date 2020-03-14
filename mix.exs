8a5cfc42b7d6720169d9881b7e1ccefd8beca08e
Mafinar Khan <mafinar@gmail.com>
2020-03-14 01:27:25 -0400

    Add CSV

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Covid19.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:csv, "~> 2.3"}
     ]
   end
 
