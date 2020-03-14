6cc7d6f250335f2125ed75a5e80795386e6a2e7b
Mafinar Khan <mafinar@gmail.com>
2020-03-14 03:06:38 -0400

    Modify read and read_all to return raw list data

--- lib/covid19/data_source/daily_csv.ex
+++ lib/covid19/data_source/daily_csv.ex
@@ -7,29 +7,32 @@ defmodule Covid19.DataSource.DailyCSV do
 
   @typep value() :: String.t()
   @type row :: %{
-    required(:confirmed) => value(),
-    required(:country) => value(),
-    required(:deaths) => value(),
-    required(:recovered) => value(),
-    required(:province_or_state) => value(),
-    optional(:latitude) => value(),
-    optional(:longitude) => value()
-  }
+          required(:confirmed) => value(),
+          required(:country) => value(),
+          required(:deaths) => value(),
+          required(:recovered) => value(),
+          required(:province_or_state) => value(),
+          optional(:latitude) => value(),
+          optional(:longitude) => value()
+        } | [value()]
+  @type parsed_content :: [row()]
 
   @doc """
   Read from a single CSV file. It takes a `Date` as input.
 
   Returns a list of map with the heading items of the CSV as keys.
   """
-  @spec read(Date.t()) :: [row()]
-  def read(%Date{} = date \\ Date.utc_today()) do
+  @spec read(Date.t(), boolean()) :: parsed_content()
+  def read(%Date{} = date, sanitized? \\ true) do
     with path <- PathHelpers.get_data_file(date),
          true <- File.exists?(path) do
-      path
-      |> File.stream!()
-      |> CSV.decode!()
-      |> Enum.to_list()
-      |> as_map()
+      data =
+        path
+        |> File.stream!()
+        |> CSV.decode!()
+        |> Enum.to_list()
+
+      (sanitized? && as_map(data)) || data
     else
       false -> {:error, :nofile}
     end
@@ -38,10 +41,10 @@ defmodule Covid19.DataSource.DailyCSV do
   @doc """
   Reads all files and stores them as a single list
   """
-  @spec read_all() :: [row()]
-  def read_all do
-    PathHelpers.dates
-    |> Enum.flat_map(&read/1)
+  @spec read_all(boolean()) :: [parsed_content()]
+  def read_all(sanitized? \\ true) do
+    PathHelpers.dates()
+    |> Enum.map(&read(&1, sanitized?))
   end
 
   defp as_map([heading | body]) do
