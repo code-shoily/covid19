a54470dc89dd40bc131c7468a44b9c804c10fa5a
Mafinar Khan <mafinar@gmail.com>
2020-03-14 03:06:49 -0400

    Add inspector and sanitize countries

---
+++ lib/covid19/helpers/inspector.ex
@@ -0,0 +1,65 @@
+defmodule Covid19.Helpers.Inspector do
+  @moduledoc """
+  Inspects CSV data to find any discrepancy that gets added to
+  the sanitizer
+  """
+  alias Covid19.DataSource.DailyCSV
+  alias Covid19.Helpers.Sanitizer
+
+  @doc """
+  Get a list of headers.
+  """
+  def headers do
+    DailyCSV.read_all(false)
+    |> Enum.flat_map(&hd/1)
+    |> Enum.uniq()
+  end
+
+  @doc """
+  Valid country names
+  """
+  def all_countries do
+    Countries.all()
+    |> Enum.map(& &1.name)
+    |> Enum.sort()
+  end
+
+  @doc """
+  Country names that have typos in the dataset.
+
+  Ideally, this list should be zero, any typos found here should
+  be properly translated in the `Sanitizer` module as soon as encountered.
+
+  For example, if `incorrect_country_names` include `"USA"`, then it means
+  we have not sanitized for `"USA"`, therefore, we must head over to `Sanitizer`
+  and add `sanitized_country("USA"), do: "United States of America"`. This will
+  not be an element of the output thereafter.
+  """
+  def incorrect_country_names do
+    get_mentioned_countries()
+    |> Enum.reject(fn country ->
+      country in all_countries()
+    end)
+  end
+
+  @doc """
+  Countries that are not once mentioned in the dataset
+  """
+  def absent_countries do
+    mentioned_countries = get_mentioned_countries()
+
+    all_countries()
+    |> Enum.reject(fn country ->
+      country in mentioned_countries
+    end)
+  end
+
+  defp get_mentioned_countries do
+    DailyCSV.read_all(true)
+    |> Enum.flat_map(& &1)
+    |> Enum.map(& &1.country)
+    |> Enum.uniq()
+    |> Enum.sort()
+    |> Enum.map(&Sanitizer.sanitize_country/1)
+  end
+end
--- lib/covid19/helpers/sanitizer.ex
+++ lib/covid19/helpers/sanitizer.ex
@@ -26,4 +26,50 @@ defmodule Covid19.Helpers.Sanitizer do
   def sanitize_heading("Province/State"), do: :province_or_state
   def sanitize_heading("Latitude"), do: :latitude
   def sanitize_heading("Longitude"), do: :longitude
+
+  @doc """
+  Translates country names found in the file into standard country names.
+  """
+  def sanitize_country("Bolivia"), do: "Bolivia (Plurinational State of)"
+  def sanitize_country("Brunei"), do: "Brunei Darussalam"
+  def sanitize_country("Congo (Kinshasa)"), do: "Congo (Democratic Republic of the)"
+  def sanitize_country("Cote d'Ivoire"), do: "Côte d'Ivoire"
+  def sanitize_country("Ivory Coast"), do: "Côte d'Ivoire"
+  def sanitize_country("Czechia"), do: "Czech Republic"
+  def sanitize_country("Vatican City"), do: "Holy See"
+  def sanitize_country("Hong Kong SAR"), do: "Hong Kong"
+  def sanitize_country("Republic of Ireland"), do: "Ireland"
+  def sanitize_country("South Korea"), do: "Korea (Democratic People's Republic of)"
+  def sanitize_country("Iran"), do: "Iran (Islamic Republic of)"
+  def sanitize_country("Korea, South"), do: "Korea (Democratic People's Republic of)"
+  def sanitize_country("Republic of Korea"), do: "Korea (Republic of)"
+  def sanitize_country("Macao SAR"), do: "Macao"
+  def sanitize_country("Macau"), do: "Macao"
+  def sanitize_country("North Macedonia"), do: "Macedonia (the former Yugoslav Republic of)"
+  def sanitize_country("Moldova"), do: "Moldova (Republic of)"
+  def sanitize_country("Republic of Moldova"), do: "Moldova (Republic of)"
+  def sanitize_country("Mainland China"), do: "China"
+
+  def sanitize_country("North Ireland"),
+    do: "United Kingdom of Great Britain and Northern Ireland"
+
+  def sanitize_country("occupied Palestinian territory"), do: "Palestine, State of"
+  def sanitize_country("Palestine"), do: "Palestine, State of"
+  def sanitize_country("Reunion"), do: "Réunion"
+  def sanitize_country("Russia"), do: "Russian Federation"
+  def sanitize_country("Saint Barthelemy"), do: "Saint Barthélemy"
+  def sanitize_country("Saint Martin"), do: "Saint Martin (French part)"
+  def sanitize_country("St. Martin"), do: "Saint Martin (French part)"
+  def sanitize_country("Taiwan"), do: "Taiwan, Province of China"
+  def sanitize_country("Taiwan*"), do: "Taiwan, Province of China"
+  def sanitize_country("Taipei and environs"), do: "Taiwan, Province of China"
+  def sanitize_country("UK"), do: "United Kingdom of Great Britain and Northern Ireland"
+
+  def sanitize_country("United Kingdom"),
+    do: "United Kingdom of Great Britain and Northern Ireland"
+
+  def sanitize_country("US"), do: "United States of America"
+  def sanitize_country("Vietnam"), do: "Viet Nam"
+
+  def sanitize_country(country), do: country
 end
