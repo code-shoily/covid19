fc6cddd13a5b4cfc571acde3a4863f7848617494
Mafinar Khan <mafinar@gmail.com>
2020-04-15 20:15:29 -0400

    Elixir 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Covid19.MixProject do
     [
       app: :covid19,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
