b96b614bcf6b7cbac9ae14810567faec96695dad
Mafinar Khan <mafinar@gmail.com>
2020-04-19 20:40:52 -0400

    Make the table container smaller

--- lib/covid19_web/live/components/countrywise_summary.ex
+++ lib/covid19_web/live/components/countrywise_summary.ex
@@ -46,63 +46,63 @@ defmodule Covid19Web.Live.Components.CountrywiseSummary do
           </div>
         </div>
 
-        <div class="table-container">
-        <table class="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>
-                <a href="#" phx-click="sort" phx-value-by="country_or_region" phx-target="<%= @myself %>">
-                  Country/Region <%= show_sort_icon(:country_or_region, @by, @dir) %>
-                </a>
-              </th>
-              <th>
-                <a href="#" phx-click="sort" phx-value-by="confirmed" phx-target="<%= @myself %>">
-                  Confirmed <%= show_sort_icon(:confirmed, @by, @dir) %>
-                </a>
-              </th>
-              <th class="has-text-right">
-                <a href="#" phx-click="sort" phx-value-by="new_confirmed" phx-target="<%= @myself %>">
-                  New Cases <%= show_sort_icon(:new_confirmed, @by, @dir) %>
-                </a>
-              </th>
-              <th>
-                <a href="#" phx-click="sort" phx-value-by="active" phx-target="<%= @myself %>">
-                  Active <%= show_sort_icon(:active, @by, @dir) %>
-                </a>
-              </th>
-              <th class="has-text-centered">
-                <a href="#" phx-click="sort" phx-value-by="recovered" phx-target="<%= @myself %>">
-                  Recovered <%= show_sort_icon(:recovered, @by, @dir) %>
-                </a>
-              </th>
-              <th>
-                <a href="#" phx-click="sort" phx-value-by="deaths" phx-target="<%= @myself %>">
-                  Deaths <%= show_sort_icon(:deaths, @by, @dir) %>
-                </a>
-              </th>
-              <th class="has-text-centered">
-                <a href="#" phx-click="sort" phx-value-by="new_deaths" phx-target="<%= @myself %>">
-                  New Deaths <%= show_sort_icon(:new_deaths, @by, @dir) %>
-                </a>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for {d, idx} <- sorted(@data, @by, @dir, @term) do %>
+        <div class="table-container" style="height: 400px; overflow: auto">
+          <table class="table is-striped is-narrow is-fullwidth">
+            <thead>
               <tr>
-                <td><%= idx %></td>
-                <td><%= d.country_or_region %></td>
-                <td class="has-text-weight-semibold"><%= d.confirmed |> fmt() %></td>
-                <td class="has-text-weight-semibold has-text-right"><%= d.new_confirmed |> fmt() %></td>
-                <td class="has-text-weight-semibold has-text-right"><%= d.active |> fmt() %></td>
-                <td class="has-background-primary has-text-weight-semibold has-text-centered"><%= d.recovered |> fmt() %></td>
-                <td class="has-text-weight-semibold"><%= d.deaths |> fmt() %></td>
-                <td class="has-background-danger has-text-weight-semibold has-text-centered"><%= d.new_deaths |> fmt() %></td>
+                <th>#</th>
+                <th>
+                  <a href="#" phx-click="sort" phx-value-by="country_or_region" phx-target="<%= @myself %>">
+                    Country/Region <%= show_sort_icon(:country_or_region, @by, @dir) %>
+                  </a>
+                </th>
+                <th>
+                  <a href="#" phx-click="sort" phx-value-by="confirmed" phx-target="<%= @myself %>">
+                    Confirmed <%= show_sort_icon(:confirmed, @by, @dir) %>
+                  </a>
+                </th>
+                <th class="has-text-right">
+                  <a href="#" phx-click="sort" phx-value-by="new_confirmed" phx-target="<%= @myself %>">
+                    New Cases <%= show_sort_icon(:new_confirmed, @by, @dir) %>
+                  </a>
+                </th>
+                <th>
+                  <a href="#" phx-click="sort" phx-value-by="active" phx-target="<%= @myself %>">
+                    Active <%= show_sort_icon(:active, @by, @dir) %>
+                  </a>
+                </th>
+                <th class="has-text-centered">
+                  <a href="#" phx-click="sort" phx-value-by="recovered" phx-target="<%= @myself %>">
+                    Recovered <%= show_sort_icon(:recovered, @by, @dir) %>
+                  </a>
+                </th>
+                <th>
+                  <a href="#" phx-click="sort" phx-value-by="deaths" phx-target="<%= @myself %>">
+                    Deaths <%= show_sort_icon(:deaths, @by, @dir) %>
+                  </a>
+                </th>
+                <th class="has-text-centered">
+                  <a href="#" phx-click="sort" phx-value-by="new_deaths" phx-target="<%= @myself %>">
+                    New Deaths <%= show_sort_icon(:new_deaths, @by, @dir) %>
+                  </a>
+                </th>
               </tr>
-            <% end %>
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              <%= for {d, idx} <- sorted(@data, @by, @dir, @term) do %>
+                <tr>
+                  <td><%= idx %></td>
+                  <td><%= d.country_or_region %></td>
+                  <td class="has-text-weight-semibold"><%= d.confirmed |> fmt() %></td>
+                  <td class="has-text-weight-semibold has-text-right"><%= d.new_confirmed |> fmt() %></td>
+                  <td class="has-text-weight-semibold has-text-right"><%= d.active |> fmt() %></td>
+                  <td class="has-background-primary has-text-weight-semibold has-text-centered"><%= d.recovered |> fmt() %></td>
+                  <td class="has-text-weight-semibold"><%= d.deaths |> fmt() %></td>
+                  <td class="has-background-danger has-text-weight-semibold has-text-centered"><%= d.new_deaths |> fmt() %></td>
+                </tr>
+              <% end %>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
