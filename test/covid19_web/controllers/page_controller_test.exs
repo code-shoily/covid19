d266ca2eaf1101fed5e8f76587f771ceaac50102
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-11-24 02:00:17 -0500

    Remove unnecessary test case

--- test/covid19_web/controllers/page_controller_test.exs
+++ test/covid19_web/controllers/page_controller_test.exs
@@ -1,8 +1,3 @@
 defmodule Covid19Web.PageControllerTest do
   use Covid19Web.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
 end
