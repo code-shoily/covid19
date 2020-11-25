910055ac5cfa6b0e161b74c09eb1f804af8e53d5
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-11-25 14:42:02 -0500

    Refactor component organization

---
+++ lib/covid19_web/live/components/case_chart_component.ex
@@ -0,0 +1,54 @@
+defmodule Covid19Web.CaseChartComponent do
+  @moduledoc false
+
+  use Covid19Web, :live_component
+
+  def mount(socket) do
+    {:ok,
+     socket
+     |> assign(is_log: false)}
+  end
+
+  def handle_event("toggle-log-chart", _, socket) do
+    {:noreply, socket |> update(:is_log, &Kernel.not/1)}
+  end
+
+  defp fmt(%Date{} = date) do
+    Timex.format!(date, "%b %d, %Y", :strftime)
+  end
+
+  def render(assigns) do
+    ~L"""
+    <div
+    class="card"
+    phx-hook="CaseChart"
+    data-statistics="<%= Jason.encode! @data %>"
+    data-log="<%= Jason.encode! @is_log %>">
+      <div class="card-content">
+        <p class="title is-5 is-uppercase has-text-centered">Confirmed Cases</p>
+        <p class="subtitle is-6 is-uppercase has-text-centered has-text-monospace">
+          <%= Enum.at(@data, 0).date |> fmt() %> -
+          <%= Enum.at(@data, -1).date |> fmt() %>
+        </p>
+        <div phx-update="ignore">
+          <p class="title is-6 is-uppercase has-text-centered">New Reports</p>
+          <div id="new-case-chart" style="height: 180px"></div>
+        </div>
+        <hr />
+        <div phx-update="ignore">
+          <p class="title is-6 is-uppercase has-text-centered">Cumulative Data</p>
+          <div id="cumulative-case-chart" style="height: 180px"></div>
+        </div>
+
+        <p class="has-text-centered has-text-monospace">
+          <button
+              class="button is-small <%= @is_log && "is-info" || "is-link" %>"
+              phx-click="toggle-log-chart" phx-target="<%= @myself %>">
+            <%= @is_log && "Show Linear" || "Show Logarithmic" %>
+          </button>
+        </p>
+      </div>
+    </div>
+    """
+  end
+end
--- lib/covid19_web/live/components/pie_chart_component.ex
+++ lib/covid19_web/live/components/pie_chart_component.ex
@@ -2,8 +2,4 @@ defmodule Covid19Web.PieChartComponent do
   @moduledoc false
 
   use Covid19Web, :live_component
-
-  defp fmt(%Date{} = date) do
-    Timex.format!(date, "%b %d, %Y", :strftime)
-  end
 end
