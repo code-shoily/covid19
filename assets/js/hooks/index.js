95acd5604d0ecb54a57dfb1447a987d23823bca2
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2020-11-27 22:44:45 -0500

    Refactor CRD charts

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,13 +1,9 @@
 import LeafletMapHook from './leaflet_map_hook'
-import CaseChartHook from './case_chart_hook'
-import DeathChartHook from './death_chart_hook'
-import RecoveredChartHook from './recovered_chart_hook'
 import PieChartHook from './pie_chart_hook'
+import CRDChartHook from './crd_chart_hook'
 
 export default {
     LeafletMap: LeafletMapHook,
-    CaseChart: CaseChartHook,
-    DeathChart: DeathChartHook,
-    RecoveredChart: RecoveredChartHook,
+    CRDChart: CRDChartHook,
     PieChart: PieChartHook
 }
---
+++ lib/covid19_web/live/components/crd_chart_component.ex
@@ -0,0 +1,56 @@
+defmodule Covid19Web.CRDChartComponent do
+  @moduledoc false
+
+  use Covid19Web, :live_component
+
+  def mount(socket) do
+    {:ok, assign(socket, logarithmic: false)}
+  end
+
+  def handle_event("toggle-log-chart", _, socket) do
+    {:noreply, update(socket, :logarithmic, &Kernel.not/1)}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <div
+      class="card"
+      phx-hook="CRDChart"
+      data-type="<%= @type %>"
+      data-statistics="<%= Jason.encode!(@data) %>"
+      data-logarithmic="<%= Jason.encode!(@logarithmic) %>">
+      <div class="card-content">
+        <p class="title is-5 is-uppercase has-text-centered"><%= @heading %></p>
+        <p class="subtitle is-6 is-uppercase has-text-centered has-text-monospace">
+          <%= format Enum.at(@data, 0).date %> - <%= format Enum.at(@data, -1).date %>
+        </p>
+        <div phx-update="ignore">
+          <p class="title is-6 is-uppercase has-text-centered">Daily</p>
+          <div id="new-<%= @type %>-chart" style="height: 180px"></div>
+        </div>
+        <hr />
+        <div phx-update="ignore">
+          <p class="title is-6 is-uppercase has-text-centered">Cumulative</p>
+          <div id="cumulative-<%= @type %>-chart" style="height: 180px"></div>
+        </div>
+
+        <p class="has-text-centered has-text-monospace">
+          <button
+              class="button is-small <%= button_class(@type) %>"
+              phx-click="toggle-log-chart" phx-target="<%= @myself %>">
+            <%= @logarithmic && "Show Linear" || "Show Logarithmic" %>
+          </button>
+        </p>
+      </div>
+    </div>
+    """
+  end
+
+  defp format(%Date{} = date) do
+    Timex.format!(date, "%b %d, %Y", :strftime)
+  end
+
+  defp button_class("confirmed"), do: "is-info"
+  defp button_class("deaths"), do: "is-danger"
+  defp button_class("recovered"), do: "is-success"
+end
