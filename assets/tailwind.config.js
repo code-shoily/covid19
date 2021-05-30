63944625c1005d07fc4c85548dd829c3306d4777
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-05-30 18:50:24 -0400

    Setup Tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    '../lib/**/*.ex',
+    '../lib/**/*.leex',
+    '../lib/**/*.eex',
+    './js/**/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -30,7 +30,7 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.s?css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
       }
     ]
   },
