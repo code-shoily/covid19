a6d2552534a2ec4e371a4f9b8c04d48b11fdae22
Mafinar Khan <mafinar@coffeemeetsbagel.com>
2021-06-14 00:36:12 -0400

    Convert to CLJS

--- assets/src/app/main.cljs
+++ assets/src/app/main.cljs
@@ -1,30 +1,33 @@
 (ns app.main
-  [:require ["nprogress" :as nprogress]
-            ["phoenix" :as phoenix]
-            ["phoenix_live_view" :as LiveSocket]])
+  [:require
+   ["nprogress" :as nprogress]
+   ["phoenix" :refer [Socket]]
+   ["phoenix_live_view" :refer [LiveSocket]]
+   [app.hooks.heat-map :refer [HeatMap]]
+   [app.hooks.crd-chart :refer [CRDChart]]
+   [app.hooks.crd-pie-chart :refer [CRDPieChart]]])
 
-(def csrf-token (.getAttribute (js/document.querySelector "meta[name='csrf-token']") "content"))
+(def csrf-token (-> "meta[name='csrf-token']" (js/document.querySelector) (.getAttribute "content")))
 (def live-socket-params
-  #js {
-    "params" #js {"_csrf_token" csrf-token}
-    "hooks" #js {
-      "HeatMap" nil
-      "CRDChart" nil
-      "CRDPieChart" nil
-    }})
+  (clj->js {"params" {"_csrf_token" csrf-token}
+            "hooks" {"HeatMap" (HeatMap.)
+                     "CRDChart" (CRDChart.)
+                     "CRDPieChart" (CRDPieChart.)}}))
 
-(set! 
-  (.. js/window -liveSocket) 
-  (LiveSocket/LiveSocket. "/live" phoenix/Socket live-socket-params))
+(set!
+ (.. js/window -liveSocket)
+ (LiveSocket. "/live" Socket live-socket-params))
 
 (def live-socket (.. js/window -liveSocket))
 
-(defn initialize []
-    (js/window.addEventListener "phx:page-loading-start" (fn [_] (.start nprogress)))
-    (js/window.addEventListener "phx:page-loading-stop" (fn [_] (.done nprogress))))
+(defn init-progress-bar []
+  (js/window.addEventListener "phx:page-loading-start" (fn [_] (.start nprogress)))
+  (js/window.addEventListener "phx:page-loading-stop" (fn [_] (.done nprogress))))
 
 (defn main! []
-  (initialize)
+  (init-progress-bar)
   (.connect live-socket))
 
-(defn reload! [] (initialize))
+(defn reload! []
+  (init-progress-bar)
+  (.connect live-socket))
