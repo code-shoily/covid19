27309c14e7471fe532b624896cac0b6dee26bb24
Mafinar K <mafinar@gmail.com>
2023-02-11 02:30:46 -0500

    Add topbar to package.json

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,7 +24,7 @@ import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import topbar from "topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
