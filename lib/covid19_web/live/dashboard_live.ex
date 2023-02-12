4d9a4572233a24ec95f776f12613a8e42c9bb827
Mafinar K <mafinar@gmail.com>
2023-02-12 01:13:46 -0500

    Add mount and remove header for now

--- lib/covid19_web/live/dashboard_live.ex
+++ lib/covid19_web/live/dashboard_live.ex
@@ -1,9 +1,21 @@
 defmodule Covid19Web.DashboardLive do
   use Covid19Web, :live_view
 
+  alias Covid19.Queries
+
+  def mount(_, _, socket) do
+    assigns = %{
+      summary: Queries.summary_by_dates(),
+      dates: Queries.dates(:global),
+      date_index: 0
+    }
+
+    {:ok, assign(socket, assigns)}
+  end
+
   def render(assigns) do
     ~H"""
-    [ COVID19 Dashboard PLACEHOLDER ]
+    Total Rows in World Summary is: <%= Enum.count(@summary) %>
     """
   end
 end
